9f693e783f8c15ef569602252197c0d3df72bab5
Juan <pront23@gmail.com>
2021-05-13 22:04:20 +0900

    completed submit launch function

--- public/js/script.js
+++ public/js/script.js
@@ -61,8 +61,23 @@ function submitLaunch() {
     upcoming: true,
   });
 
-  // TODO: Once API is ready.
-  // Submit above data to launch system and reload launches.
+  return fetch('/v1/launches', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      launchDate: Math.floor(launchDate / 1000),
+      flightNumber,
+      mission,
+      rocket,
+      target,
+    }),
+  })
+    .then(() => {
+      document.getElementById('launch-success').hidden = false;
+    })
+    .then(loadLaunches);
 }
 
 function listUpcoming() {
