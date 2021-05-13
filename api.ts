fa10cec08d7bc192f905c42e4511be1ff85ab735
Juan <pront23@gmail.com>
2021-05-13 17:01:29 +0900

    added /planets endpoint

--- api.ts
+++ api.ts
@@ -1,4 +1,5 @@
 import { Router } from 'https://deno.land/x/oak@v7.4.1/mod.ts';
+import * as planets from './models/planets.ts';
 
 const router = new Router();
 
@@ -11,6 +12,10 @@ router.get('/', (ctx) => {
     {__    {_ {__  {__     {__ {__    {__ {__     {__
     {__       {__ {__       {__  {__ __  {__       {__
                      Mission Control API`;
+});
+
+router.get('/planets', (ctx) => {
+  ctx.response.body = planets.getAllPlanets();
 });
 
 export default router;
