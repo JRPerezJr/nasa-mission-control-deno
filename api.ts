a23187fe19edd7bd357ff1ed6d8daf05f82185c9
Juan <pront23@gmail.com>
2021-05-13 19:50:10 +0900

    added launches endpoint

--- api.ts
+++ api.ts
@@ -1,5 +1,6 @@
 import { Router } from 'https://deno.land/x/oak@v7.4.1/mod.ts';
 import * as planets from './models/planets.ts';
+import * as launches from './models/launches.ts';
 
 const router = new Router();
 
@@ -17,6 +18,10 @@ router.get('/', (ctx) => {
 router.get('/v1/planets', (ctx) => {
   // ctx.throw(501, 'Sorry no planets available!');
   ctx.response.body = planets.getAllPlanets();
+});
+
+router.get('/v1/launches', (ctx) => {
+  ctx.response.body = launches.getAll();
 });
 
 export default router;
