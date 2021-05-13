ff0238fe71826ef652da4e392453447862fd40da
Juan <pront23@gmail.com>
2021-05-13 18:40:47 +0900

    added logger functions

--- mod.ts
+++ mod.ts
@@ -1,8 +1,22 @@
+import * as log from 'https://deno.land/std@0.96.0/log/mod.ts';
+
 import { Application, send } from 'https://deno.land/x/oak@v7.4.1/mod.ts';
 import api from './api.ts';
 
 const app = new Application();
 const PORT = 8000;
+
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler('INFO'),
+  },
+  loggers: {
+    default: {
+      level: 'INFO',
+      handlers: ['console'],
+    },
+  },
+});
 
 app.use(async (ctx, next) => {
   await next();
